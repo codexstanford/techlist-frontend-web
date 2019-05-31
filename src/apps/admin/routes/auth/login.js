7ba17d8424a9f82b7f5d8512a166fdc1b9cc6fec
Michael Edelman <michael@edel.mn>
2019-05-31 11:39:21 -0400

    hotfix: add default export to login component

--- src/apps/admin/routes/auth/login.js
+++ src/apps/admin/routes/auth/login.js
@@ -32,7 +32,7 @@ function Login({ classes, location, ...props }) {
   } else if (user && user.person && user.person.profile !== null) {
     navigate('/app/profile/', { state: { data } });
   } else {
-    // navigate('/app/profile'); 
+    // navigate('/app/profile');
   }
 
   function handleLoginSubmit(values, { setSubmitting }) {
@@ -121,3 +121,5 @@ function Login({ classes, location, ...props }) {
     </Formik>
   );
 }
+
+export default Login;
