7f3a60b4632c886d1e59f9568f88f83b205ceb2b
Michael Edelman <michael@fabulas.io>
2019-07-04 09:10:41 -0400

    feat: add generic confirm dialog component
    Closes #34

---
+++ src/atoms/confirm.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args));
+
+const Transition = React.forwardRef(function Transition(
+  { direction = 'up', ...props },
+  ref
+) {
+  return <Slide direction={direction} ref={ref} {...props} />;
+});
+
+export function Confirm({
+  cancelText = 'Cancel',
+  confirmText = 'Confirm',
+  direction = 'up',
+  onConfirm = () => {},
+  onCancel = () => {},
+  onClose = () => {},
+  open = true,
+  title = 'Are you sure?',
+  toggle = () => {},
+  subtitle = 'Confirm that you really want to do this',
+  ...props
+}) {
+  const [isOpen, setOpen] = React.useState(false);
+
+  function handleCancel() {
+    callAll(onCancel(), setOpen(false), onClose());
+  }
+
+  function handleConfirm() {
+    callAll(onConfirm(), setOpen(false), onClose());
+  }
+
+  React.useEffect(() => {
+    if (open && typeof open === 'boolean') {
+      setOpen(open);
+    }
+  }, [open]);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={handleCancel}
+      TransitionComponent={Transition}
+      TransitionProps={{ direction }}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {subtitle}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleConfirm} color="primary">
+          {confirmText}
+        </Button>
+        <Button onClick={handleCancel} color="primary" autoFocus>
+          {cancelText}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+Confirm.propTypes = {
+  open: PropTypes.bool,
+  direction: PropTypes.oneOf(['up', 'down', 'left', 'right']),
+  onConfirm: PropTypes.func.isRequired,
+  confirmText: PropTypes.string,
+  cancelText: PropTypes.string,
+  onCancel: PropTypes.func.isRequired,
+  onClose: PropTypes.func,
+  title: PropTypes.element,
+  subtitle: PropTypes.element,
+};
+
+export default Confirm;
