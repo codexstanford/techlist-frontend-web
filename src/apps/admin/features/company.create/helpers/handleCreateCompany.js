af09a85a759131ecc45a8424562c612bf195ad7e
TerrellLinnell <terrell.linnell@gmail.com>
2019-07-09 11:35:25 -0600

    chore: Pretty prinit company categories
    Create formatCatagory function to format company categories going in and out
    
    #66

--- src/apps/admin/features/company.create/helpers/handleCreateCompany.js
+++ src/apps/admin/features/company.create/helpers/handleCreateCompany.js
@@ -25,7 +25,6 @@ export function handleCreateCompany(props) {
     { setSubmitting }
   ) => {
     const { formatted_address, geometry, place_id } = locationjson;
-    console.log('called!', logo);
 
     try {
       const result = await mutation({
