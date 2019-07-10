dd573fa47e61097e67a3a3e972ab8366112ea12b
TerrellLinnell <terrell.linnell@gmail.com>
2019-07-10 15:10:00 -0600

    chore: Make create company form mobile friendly
    Made all sections responsive to screen size changes, this includes a change up in the preview window, fixed form validation issue where touching some fields did not set them as touched fields, categories had to use onFocus instead of onBlur, reworked form level error to look better with long error messages
    
    #73

--- src/apps/admin/features/company.create/components/basics.js
+++ src/apps/admin/features/company.create/components/basics.js
@@ -2,68 +2,73 @@ import React from 'react';
 import CodeXTextField from '../../../components/codex.textinput';
 import CompanyTargetMarketSelect from './select';
 import { Field } from 'formik';
+import styled from 'styled-components';
+import { StylesProvider } from '@material-ui/styles';
 
 export function Basics({ errors, touched, classes, targetMarkets, ...rest }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-      }}
-    >
-      <CodeXTextField
-        name="name"
-        type="text"
-        errors={errors}
-        touched={touched}
-        label="Name"
-        fullWidth={true}
-      />
-      <CodeXTextField
-        type="text"
-        name="description"
-        multiline={true}
-        margin="normal"
-        errors={errors}
-        touched={touched}
-        label="Description"
-      />
-      <div style={{ display: 'flex' }}>
+    <StylesProvider injectFirst>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+        }}
+      >
         <CodeXTextField
-          name="yearFounded"
-          margin="normal"
-          type="date"
+          name="name"
+          type="text"
           errors={errors}
           touched={touched}
-          label="Date Founded"
-          fullWidth={false}
+          label="Name"
+          fullWidth={true}
         />
-        <div>
-          <Field
-            name="targetMarkets"
-            component={CompanyTargetMarketSelect}
-            classes={classes}
-            options={
-              targetMarkets &&
-              targetMarkets.organizationTargetMarkets &&
-              targetMarkets.organizationTargetMarkets.length > 0
-                ? targetMarkets.organizationTargetMarkets.map(t => ({
-                    type: t.id,
-                    niceName: t.payload,
-                  }))
-                : []
-            }
-            label="Target Markets"
-            styles={{
-              paddingRight: '1rem',
-              minWidth: '150px',
-              marginTop: '15px',
-              marginLeft: '2rem',
-            }}
+        <CodeXTextField
+          type="text"
+          name="description"
+          multiline={true}
+          margin="normal"
+          errors={errors}
+          touched={touched}
+          label="Description"
+        />
+        <YearAndMarketLayout>
+          <CodeXTextField
+            name="yearFounded"
+            margin="normal"
+            type="date"
+            errors={errors}
+            touched={touched}
+            label="Date Founded"
+            fullWidth={false}
           />
-        </div>
+          <div>
+            <Field
+              name="targetMarkets"
+              component={CompanyTargetMarketSelect}
+              classes={classes}
+              options={
+                targetMarkets &&
+                targetMarkets.organizationTargetMarkets &&
+                targetMarkets.organizationTargetMarkets.length > 0
+                  ? targetMarkets.organizationTargetMarkets.map(t => ({
+                      type: t.id,
+                      niceName: t.payload,
+                    }))
+                  : []
+              }
+              label="Target Markets"
+            />
+          </div>
+        </YearAndMarketLayout>
       </div>
-    </div>
+    </StylesProvider>
   );
 }
+
+const YearAndMarketLayout = styled.div`
+  display: flex;
+  @media (max-width: 480px) {
+    flex-direction: column;
+  }
+`;
