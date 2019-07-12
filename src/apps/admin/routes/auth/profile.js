be58d84aed7b10b6a938ee28ee3d25b690b40016
Tim Myers <tim@drivenwithdata.com>
2019-07-12 10:08:23 -0600

    add paper to auth/profile component

--- src/apps/admin/routes/auth/profile.js
+++ src/apps/admin/routes/auth/profile.js
@@ -9,6 +9,7 @@ import styled from 'styled-components';
 import { steps } from '../../../../helpers/enums';
 import { TextField, Select } from 'formik-material-ui';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
 import { MenuItem } from '@material-ui/core';
 
 import Fab from '@material-ui/core/Fab';
@@ -118,206 +119,210 @@ function CreateProfile({ classes, ...props }) {
       }) => {
         return (
           <Container className={classes.main}>
-            <HeaderWrapper>
-              <Typography
-                variant="h5"
-                color="primary"
-                style={{
-                  fontWeight: '700',
-                  letterSpacing: '-.5px',
-                  textDecoration: 'none',
-                }}
-              >
-                Create CodeX Profile
-              </Typography>
-            </HeaderWrapper>
-            <Form className={classes.form}>
-              <div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <StyledInput
-                    onChange={e => {
-                      e.stopPropagation();
-                      e.preventDefault();
-                      console.log(e.target.files);
+            <Paper className={classes.paper}>
+              <HeaderWrapper>
+                <Typography
+                  variant="h5"
+                  color="primary"
+                  style={{
+                    fontWeight: '700',
+                    letterSpacing: '-.5px',
+                    textDecoration: 'none',
+                  }}
+                >
+                  Create CodeX Profile
+                </Typography>
+              </HeaderWrapper>
+              <Form className={classes.form}>
+                <div>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <StyledInput
+                      onChange={e => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        console.log(e.target.files);
 
-                      const fileReader = new FileReader();
-                      fileReader.onloadend = e => {
-                        const content = fileReader.result;
-                        setFieldValue('avatar', content);
-                        setImage(content);
-                      };
-                      if (e.target.files.length > 0) {
-                        fileReader.readAsDataURL(e.target.files[0]);
-                      }
-                    }}
-                  />
-                  <label htmlFor="avatar">
-                    <Fab
-                      style={{
-                        margin: 10,
-                        width: 120,
-                        height: 120,
+                        const fileReader = new FileReader();
+                        fileReader.onloadend = e => {
+                          const content = fileReader.result;
+                          setFieldValue('avatar', content);
+                          setImage(content);
+                        };
+                        if (e.target.files.length > 0) {
+                          fileReader.readAsDataURL(e.target.files[0]);
+                        }
                       }}
-                    >
-                      <label htmlFor="avatar">
-                        <Avatar
-                          style={{ width: 120, height: 120 }}
-                          src={values.avatar}
-                          imgProps={{
-                            style: { maxWidth: '100%', maxHeight: '100%' },
-                          }}
-                        />
-                      </label>
-                    </Fab>
-                  </label>
-                </div>
-                <div>
-                  <Field
-                    name="firstName"
-                    type="text"
-                    label="First Name"
-                    fullWidth
-                    component={TextField}
-                  />
-                  {errors.firstName && touched.firstName ? (
-                    <div>{errors.firstName}</div>
-                  ) : null}
-                </div>
+                    />
+                    <label htmlFor="avatar">
+                      <Fab
+                        style={{
+                          margin: 10,
+                          width: 120,
+                          height: 120,
+                        }}
+                      >
+                        <label htmlFor="avatar">
+                          <Avatar
+                            style={{ width: 120, height: 120 }}
+                            src={values.avatar}
+                            imgProps={{
+                              style: { maxWidth: '100%', maxHeight: '100%' },
+                            }}
+                          />
+                        </label>
+                      </Fab>
+                    </label>
+                  </div>
+                  <div>
+                    <Field
+                      name="firstName"
+                      type="text"
+                      label="First Name"
+                      fullWidth
+                      component={TextField}
+                    />
+                    {errors.firstName && touched.firstName ? (
+                      <div>{errors.firstName}</div>
+                    ) : null}
+                  </div>
 
-                <div>
-                  <Field
-                    name="lastName"
-                    type="text"
-                    label="Last Name"
-                    fullWidth
-                    component={TextField}
-                  />
-                  {errors.lastName && touched.lastName ? (
-                    <div>{errors.lastName}</div>
-                  ) : null}
-                </div>
-                <div>
-                  <Field
-                    name="handle"
-                    type="text"
-                    label="Username"
-                    fullWidth
-                    component={TextField}
-                  />
-                  {errors.handle && touched.handle ? (
-                    <div>{errors.handle}</div>
-                  ) : null}
-                </div>
-                <div style={{ paddingTop: '1rem', paddingBottom: '1rem' }}>
-                  <Divider variant="middle" />
-                </div>
+                  <div>
+                    <Field
+                      name="lastName"
+                      type="text"
+                      label="Last Name"
+                      fullWidth
+                      component={TextField}
+                    />
+                    {errors.lastName && touched.lastName ? (
+                      <div>{errors.lastName}</div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <Field
+                      name="handle"
+                      type="text"
+                      label="Username"
+                      fullWidth
+                      component={TextField}
+                    />
+                    {errors.handle && touched.handle ? (
+                      <div>{errors.handle}</div>
+                    ) : null}
+                  </div>
+                  <div style={{ paddingTop: '1rem', paddingBottom: '1rem' }}>
+                    <Divider variant="middle" />
+                  </div>
 
-                <div>
-                  <FieldArray
-                    name="links"
-                    render={arrayHelpers => {
-                      return (
-                        <div>
-                          {values.links && values.links.length > 0 ? (
-                            values.links.map((link, index) => {
-                              return (
-                                <div
-                                  style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                  }}
-                                >
+                  <div>
+                    <FieldArray
+                      name="links"
+                      render={arrayHelpers => {
+                        return (
+                          <div>
+                            {values.links && values.links.length > 0 ? (
+                              values.links.map((link, index) => {
+                                return (
                                   <div
-                                    key={index}
                                     style={{
                                       display: 'flex',
-                                      flex: 1,
                                       justifyContent: 'space-between',
                                       alignItems: 'center',
                                     }}
                                   >
-                                    <Field
-                                      select
-                                      component={props => (
-                                        <TextField {...props} />
-                                      )}
-                                      label="Link"
-                                      name={`links.${index}.type`}
+                                    <div
+                                      key={index}
+                                      style={{
+                                        display: 'flex',
+                                        flex: 1,
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                      }}
                                     >
-                                      {linkOptions.map(opt => {
-                                        return (
-                                          <MenuItem
-                                            key={opt.value}
-                                            value={opt.value}
-                                          >
-                                            {opt.label}
-                                          </MenuItem>
-                                        );
-                                      })}
-                                    </Field>
-                                    <Field
-                                      name={`links.${index}.url`}
-                                      label="Url"
-                                      component={TextField}
-                                    />
+                                      <Field
+                                        select
+                                        component={props => (
+                                          <TextField {...props} />
+                                        )}
+                                        label="Link"
+                                        name={`links.${index}.type`}
+                                      >
+                                        {linkOptions.map(opt => {
+                                          return (
+                                            <MenuItem
+                                              key={opt.value}
+                                              value={opt.value}
+                                            >
+                                              {opt.label}
+                                            </MenuItem>
+                                          );
+                                        })}
+                                      </Field>
+                                      <Field
+                                        name={`links.${index}.url`}
+                                        label="Url"
+                                        component={TextField}
+                                      />
+                                    </div>
+                                    <div
+                                      style={{
+                                        display: 'flex',
+                                        justifyContent: 'flex-end',
+                                      }}
+                                    >
+                                      <Button
+                                        type="button"
+                                        onClick={() =>
+                                          arrayHelpers.remove(index)
+                                        }
+                                      >
+                                        <DeleteIcon />
+                                      </Button>
+                                      <Button
+                                        type="button"
+                                        onClick={() =>
+                                          arrayHelpers.insert(index, {
+                                            type: '',
+                                            url: '',
+                                            isPublic: true,
+                                          })
+                                        }
+                                      >
+                                        <AddIcon />
+                                      </Button>
+                                    </div>
                                   </div>
-                                  <div
-                                    style={{
-                                      display: 'flex',
-                                      justifyContent: 'flex-end',
-                                    }}
-                                  >
-                                    <Button
-                                      type="button"
-                                      onClick={() => arrayHelpers.remove(index)}
-                                    >
-                                      <DeleteIcon />
-                                    </Button>
-                                    <Button
-                                      type="button"
-                                      onClick={() =>
-                                        arrayHelpers.insert(index, {
-                                          type: '',
-                                          url: '',
-                                          isPublic: true,
-                                        })
-                                      }
-                                    >
-                                      <AddIcon />
-                                    </Button>
-                                  </div>
-                                </div>
-                              );
-                            })
-                          ) : (
-                            <React.Fragment />
-                          )}
-                        </div>
-                      );
-                    }}
-                  />
-                </div>
-                <ButtonWrapper>
-                  <Button
-                    type="submit"
-                    disabled={!isValid}
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                  >
-                    Create Profile!
-                  </Button>
-                  {isSubmitting && (
-                    <CircularProgress
-                      size={24}
-                      className={classes.buttonProgress}
+                                );
+                              })
+                            ) : (
+                              <React.Fragment />
+                            )}
+                          </div>
+                        );
+                      }}
                     />
-                  )}
-                </ButtonWrapper>
-              </div>
-            </Form>
+                  </div>
+                  <ButtonWrapper>
+                    <Button
+                      type="submit"
+                      disabled={!isValid}
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                    >
+                      Create Profile!
+                    </Button>
+                    {isSubmitting && (
+                      <CircularProgress
+                        size={24}
+                        className={classes.buttonProgress}
+                      />
+                    )}
+                  </ButtonWrapper>
+                </div>
+              </Form>
+            </Paper>
           </Container>
         );
       }}
